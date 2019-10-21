5b1f8bdd3e20266981230aae5bc04f2f1cbdc3dc
Bjørn Lau Jørgensen <bjornlau@Bjrns-MacBook-Pro.local>
2019-10-21 13:47:07 +0200

    Drastically improved code flow.

--- algorithm.playground/Contents.swift
+++ algorithm.playground/Contents.swift
@@ -3,7 +3,7 @@ struct Person {
     var hometown: String
     var age: Int
     var interests: [String: String] = [:]
-    var id: Int
+    var id: Int //  Must be a unique Int.
 }
 
 struct Results {
@@ -14,56 +14,40 @@ struct Results {
     var id2: Int
 }
 
-
-//  NOTE TO SELF: Fjerne match personerne fra resultatet og find næste match.
-var results = [Results]()
-
 var persons = [Person(name: "Jack Sparrow", hometown: "Caribbean", age: 42, interests: ["Rum": "An alcoholic drink", "Guns": "A weapon"], id: 10),
                Person(name: "Bjørn Lau Jørgensen", hometown: "Aalborg", age: 25, interests: ["Kitesurfing": "A sport using a kite", "Tech": "Technoligy", "Apple": "Computer"], id: 11),
                Person(name: "Steve Jobs", hometown: "Silicon Vally", age: 52, interests: ["Tech": "Tehcnoligy", "Mercedes": "A car", "Apple": "Computer", "Kitesurfing": "A sport using a kite"], id: 12),
                Person(name: "Bruce Dickinson", hometown: "London", age: 55, interests: ["Music" : "Songs etc.", "Tech":"Tehcnoligy", "Football":"A stupid game"], id: 13),
                Person(name: "Petra Göransson", hometown: "Kungsäter", age: 22, interests: ["Fitness":"Going to the gym", "Music":"Songs etc.", "Tech":"Technoligy"], id: 14),
                Person(name: "Jan Jørgensen", hometown: "Gjøl", age: 57, interests: ["Kitesurfing" : "A sport using a kite", "Guns":"A weapon"], id: 15),
-               Person(name: "Jan Jensen", hometown: "Aalborg", age: 50, interests: ["Kitesurfing" : "A sport using a kite", "Guns":"A weapon"], id: 16)]
+               Person(name: "Kasper Pedersen", hometown: "Aalborg", age: 50, interests: ["Kitesurfing" : "A sport using a kite", "Guns":"A weapon"], id: 16)]
 
-//  Takes the 'results' array and sorts it based on the 'rank' property in the 'Results' objects.
-//  Then prints the result and empties 'result' array. It also removes the two winning persons from the 'persons' array.
-//  At last calls the 'compareInterest()' if there is still persons to compare.
-func orderResult() {
-    let shuffeledResult = results.shuffled()
-    let sortedResult = shuffeledResult.sorted(by: {$0.rank > $1.rank})
-    let topMatch = sortedResult[0]
+
+//  Accepts an array of Results objects.
+//  Shuffels the result array then sorts based on the rank property.
+//  Selects the top match and removes the two persons all places they appear in the result array.
+func orderResult(resultObjects: [Results]) {
+    let shuffeledResult = resultObjects.shuffled()
+    var sortedResult = shuffeledResult.sorted(by: {$0.rank > $1.rank})
     
-    print("\nMATCH! \(topMatch.compared.keys) and \(topMatch.compared.values) have \(topMatch.rank) different interrests! They are: \(topMatch.differentInterests)\n")
-    
-    if let index = persons.firstIndex(where: { $0.id == sortedResult[0].id1 }) {
-        persons.remove(at: index)
-    }
-    
-    if let index = persons.firstIndex(where: { $0.id == sortedResult[0].id2 }) {
-        persons.remove(at: index)
-    }
-    
-    results.removeAll()
-    
-    if persons.isEmpty {
-        print("Everyone is matched! Happy mingling :)")
-    } else if persons.count == 1 {
-        print("Everyone is matched! Except \(persons[0].name) due to an uneven numbers of participants... Happy mingling :)")
-    } else {
-        compareInterest(personObjects: persons)
+    while sortedResult.isEmpty == false  {
+        let topMatch = sortedResult[0]
+        
+        let updatedResults = sortedResult.filter(){$0.id1 != topMatch.id1 && $0.id1 != topMatch.id2 && $0.id2 != topMatch.id1 && $0.id2 != topMatch.id2}
+        sortedResult = updatedResults
+        
+        print("\nMATCH! \(topMatch.compared.keys) and \(topMatch.compared.values) have \(topMatch.rank) different interrests! They are: \(topMatch.differentInterests)")
     }
 }
 
-
-//  Compare the interests of the persons provided to the function.
-//  The function will add the result to the 'results' array as 'Result' objects.
-func compareInterest(personObjects: [Person]) {
+//  Accepts an array of Person objects.
+//  Compares all persons interests agains each other.
+//  Outputs the result as an array of Results obejcts.
+func compareInterest(personObjects: [Person]) -> [Results] {
+    var results = [Results]()
     
     for personCount in 0..<persons.count {
         
-        //  Loop through all the persons except the first person in the array to
-        //  compare every person to the first person in array.
         for i in 0..<persons.count {
             
             let mainPerson = personObjects[personCount]
@@ -91,7 +75,7 @@ func compareInterest(personObjects: [Person]) {
             }
         }
     }
-    orderResult()
+    return results
 }
 
-compareInterest(personObjects: persons)
+orderResult(resultObjects: compareInterest(personObjects: persons))
